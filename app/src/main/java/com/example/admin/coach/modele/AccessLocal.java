e3b73902a300147a3ac7d8796ee9429b8c70f22b
admin <ines.merrouch@gmail.com>
2016-11-30 16:56:32 +0100

    [30/11] Page 37 ─ MySqlLite
    - Création de la classe AccessLocal jusqu'à bd.execSQL(req) de la méthode ajout.

---
+++ app/src/main/java/com/example/admin/coach/modele/AccessLocal.java
@@ -0,0 +1,42 @@
+package com.example.admin.coach.modele;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.admin.coach.outils.MySQLiteOpenHelper;
+
+/**
+ * Created by admin on 30/11/2016.
+ */
+
+public class AccessLocal {
+
+    // === Propriétés
+    private String nomBase = "bdCoach.sqlite" ;
+    private Integer versionBase = 1 ;
+    private MySQLiteOpenHelper accesBD ;
+    private SQLiteDatabase bd ;
+    private String req ;
+
+    /**
+     * === CONSTRUCTEUR DE LA CLASSE ACCESSLOCAL
+     * @param contexte
+     */
+    public AccessLocal(Context contexte) {
+        this.accesBD = new MySQLiteOpenHelper(contexte, nomBase, versionBase) ;
+    }
+
+    /**
+     * Ajout d'un profil dans la table Profil
+     * @param profil
+     */
+    public void ajout(Profil profil) {
+        this.bd = accesBD.getWritableDatabase() ; // Accès en écriture sur la BD
+        this.req += " (\"" + profil.getDateMesure()
+                    + "\"," + profil.getPoids()
+                    + "," + profil.getTaille()
+                    + "," + profil.getAge()
+                    + "," + profil.getSexe() + ")"; // Requête d'insertion
+        bd.execSQL(req); // Exécution de la requête
+    }
+}
